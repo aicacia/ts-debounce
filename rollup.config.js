d4aec0851175796b994cf3eaf6e1025250b5b0b7
Nathan Faucett <nathanfaucett@gmail.com>
2021-08-31 12:15:22 -0400

    single file esm output

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import { dirname } from "path";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
+import esmImportToUrl from "rollup-plugin-esm-import-to-url";
 import pkg from "./package.json";
 
 export default [
@@ -10,14 +10,18 @@ export default [
     input: "src/index.ts",
     output: [
       {
-        dir: dirname(pkg.module),
+        file: pkg.module,
         format: "es",
         sourcemap: true,
-        preserveModules: true,
         plugins: [terser()],
       },
     ],
     plugins: [
+      esmImportToUrl({
+        imports: {
+          tslib: "https://unpkg.com/tslib@2/tslib.es6.js",
+        },
+      }),
       resolve({ browser: true }),
       commonjs(),
       typescript({
