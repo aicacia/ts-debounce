bfb640cb122c03e9f27a353a6af31110394c5dee
Nathan Faucett <nathanfaucett@gmail.com>
2022-01-12 20:19:21 -0500

    add cancel and flush

--- src/debounce.ts
+++ src/debounce.ts
@@ -6,39 +6,68 @@ export interface IOptions {
 
 const noop = () => undefined;
 
+export type DebounceFn<F extends (...args: any[]) => void> = F & {
+  cancel(): void;
+  flush(): void;
+};
+
 export function debounce<F extends (...args: any[]) => void>(
   func: F,
   waitMilliseconds = 0,
   options: IOptions = {}
-): F {
+): DebounceFn<F> {
   let timeoutId: any = null,
     running = false;
 
   const before = options.before || noop,
     after = options.after || noop;
 
-  return function debounceFn<T>(this: T, ...args: any[]) {
+  function clear() {
+    if (timeoutId != null) {
+      clearTimeout(timeoutId);
+      timeoutId = null;
+    }
+  }
+
+  function cancel() {
+    running = false;
+    clear();
+  }
+
+  function flush() {
+    clear();
+    call();
+  }
+
+  function call<T>(this: T, ...args: any[]) {
+    running = false;
+    func.apply(this, args);
+    after();
+  }
+
+  const debounceFn: DebounceFn<F> = function debounceFn<T>(
+    this: T,
+    ...args: any[]
+  ) {
     if (!running) {
       running = true;
       before();
     }
 
-    if (timeoutId != null) {
-      clearTimeout(timeoutId);
-      timeoutId = null;
-    }
+    clear();
 
     if (!!options.isImmediate && timeoutId == null) {
-      running = false;
-      func.apply(this, args);
-      after();
+      call(this, args);
     } else {
       timeoutId = setTimeout(() => {
-        running = false;
         timeoutId = null;
-        func.apply(this, args);
-        after();
+        call(this, args);
       }, waitMilliseconds);
     }
   } as any;
+
+  debounceFn.cancel = cancel;
+  debounceFn.flush = flush;
+
+  return debounceFn;
 }
